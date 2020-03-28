15674ff103531330fb67c01fe608adef1dc8bcd3
gionnimontana <gionnimontana@gionnimontana.gmail.com>
2020-03-28 17:29:35 +0100

    commit build

---
+++ build/precache-manifest.74fff1e918a307ec394c9528054979ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0874d115d53f7771c48020303937ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "673a4ef0220443264ad8",
+    "url": "/static/css/main.84854289.chunk.css"
+  },
+  {
+    "revision": "836e84da5ace5399ee5e",
+    "url": "/static/js/2.78bac1a4.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/static/js/2.78bac1a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673a4ef0220443264ad8",
+    "url": "/static/js/main.7ea0f211.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
